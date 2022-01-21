fee1a425bde151cd7e18128af26160d4bccfef1b
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-01-22 01:11:37 +0530

    Eleventh commit

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -8,7 +8,7 @@ export default function Header(props) {
         <div className="sticky-top mb-3">
             <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
                 <div className="container-fluid">
-                    <Link className="navbar-brand titleHeader" to="/">{props.name}</Link>
+                    <Link className="navbar-brand title" to="/">{props.name}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
